ea633d2118d59e3d56204b55e2508b0c82ca0f67
James Morgan <james.morgan.e@googlemail.com>
2012-01-15 11:32:40 +0000

    updated TODO

--- src/main/java/com/morgan/design/TODO.java
+++ src/main/java/com/morgan/design/TODO.java
@@ -108,8 +108,8 @@ public class TODO {
 	// ///////////////////////////////////
 	
 	// FIXME -> On first successful weather loaded, present user with rate me link to market.
-	// FIXME -> Create Application Icon
-	
+
+	// FIXME -> DONE - (15/01/2012) - Create Application Icon
 	// FIXME -> DONE - (15/01/2012) - Clean up home screen
 	// FIXME -> DONE - (15/01/2012) - Confirm Google Analytics integration complete, make sure present on all areas possible, about, feedback form, all menu items
 	// FIXME -> DONE - (15/01/2012) - Add menu to overview screen
